6c979799584c7608a3188cede994f17fad517905
michael-lugashi <89575620+michael-lugashi@users.noreply.github.com>
2021-11-05 15:55:13 +0200

    set up basic webpack

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development', //production
+  entry: {
+    main: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 3000,
+    open: true,
+    hot: true,
+  },
+  // loaders
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.(svg|ico|png|webp|gif|jpeg)$/, type: 'asset/resource' }
+    ],
+  },
+  // plugins
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'URL Shortner',
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/template.html'),
+    }),
+  ],
+};
